ff5b4fded4acdb5fade703557462233043da2c76
Don Smith <locksmithdon@gmail.com>
2016-05-26 21:25:30 +1200

    Removed multer

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var hbs = require('express-handlebars')
-var multer = require('multer')
-var upload = multer()
 
 var routes = require('./routes')
 
@@ -13,10 +11,10 @@ app.engine('hbs', hbs({
   extname: 'hbs'
 }))
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded())
 
 app.get('/', routes.index)
-app.post('/create-quote', upload.array(), routes.createQuote)
+app.post('/create-quote', routes.createQuote)
 app.get('/list', routes.list)
 
 app.listen(PORT, function () {
